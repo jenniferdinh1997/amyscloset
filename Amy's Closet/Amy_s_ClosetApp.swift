ec2cd3da86bd49df2de6e281459ed8407845e085
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2023-11-02 14:43:53 -0700

    Initial Commit

---
+++ Amy's Closet/Amy_s_ClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amy_s_ClosetApp.swift
+//  Amy's Closet
+//
+//  Created by Jennifer Dinh on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Amy_s_ClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Amy's Closet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Amy's Closet
+//
+//  Created by Jennifer Dinh on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
